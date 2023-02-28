fa631c1ccd7bb8216275478de3b6ee66b8c8f19a
Rahman1000 <auliarahmanm100@gmail.com>
2023-02-28 15:18:03 +0700

    upload praktik 2

---
+++ Praktikum2/proses_nilai.php
@@ -0,0 +1,19 @@
+<?php 
+$nama = $_GET['name'];
+$matkul = $_GET['matkul'];
+$UTS = $_GET['UTS'];
+$UAS = $_GET['UAS'];
+$TUGAS = $_GET['TUGAS'];
+$total_nilai = ($UTS + $UAS + $TUGAS)/3;
+
+if($total_nilai > 75){
+    $keterangan = "Lulus";
+}else{
+    $keterangan = "Tidak Lulus";
+}
+
+
+
+
+
+?>
