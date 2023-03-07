507c24422ff9f70761adf4da592447c127deadb9
Rahman1000 <auliarahmanm100@gmail.com>
2023-03-07 17:10:18 +0700

    praktik 3

---
+++ Praktikum3/form_registrasi.php
@@ -0,0 +1,119 @@
+<?php require_once "proses_registrasi.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Registrasi</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Form Input IT Club GDSC</h2>
+
+        <form method="POST">
+            <div class="form-group row">
+                <label for="nim" class="col-4 col-form-label">NIM</label> 
+                <div class="col-8">
+                <input id="nim" name="nim" placeholder="Masukkan NIM" type="text" class="form-control" required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nama" class="col-4 col-form-label">Nama</label> 
+                <div class="col-8">
+                <input id="nama" name="nama" placeholder="Masukkan Nama" type="text" class="form-control" required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-4">Jenis Kelamin</label> 
+                <div class="col-8">
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input name="gender" id="gender_0" type="radio" required="required" class="custom-control-input" value="Pria"> 
+                    <label for="gender_0" class="custom-control-label">Pria</label>
+                </div>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input name="gender" id="gender_1" type="radio" required="required" class="custom-control-input" value="Wanita"> 
+                    <label for="gender_1" class="custom-control-label">Wanita</label>
+                </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="prodi" class="col-4 col-form-label">Program Studi</label> 
+                <div class="col-8">
+                <select id="prodi" name="prodi" class="custom-select" required="required">
+                    <?php foreach($prodi as $prodi => $value){ ?>
+                        <option value="<?= $prodi ?>"><?= $value ?></option>
+                    <?php } ?>
+                </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-4">Skill Web &amp; Programming</label> 
+                <div class="col-8">
+                    <?php foreach($skill as $key => $value){ ?>
+                        <div class="custom-control custom-checkbox custom-control-inline">
+                            <input name="skill[]" id="skill_<?= $key ?>" type="checkbox" class="custom-control-input" value="<?= $key ?>"> 
+                            <label for="skill_<?= $key ?>" class="custom-control-label"><?= $key ?></label>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="domisili" class="col-4 col-form-label">Domisili</label> 
+                <div class="col-8">
+                <select id="domisili" name="domisili" class="custom-select" required="required">
+                    <?php foreach ($domisili as $dom) { ?>
+                            <option value="<?= $dom?>"><?=$dom?></option>
+                    <?php } ?>
+                </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-4 col-form-label">Email</label> 
+                <div class="col-8">
+                <input id="email" name="email" placeholder="Masukkan Email" type="text" class="form-control" required="required">
+                </div>
+            </div> 
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <table class="table table-bordered">
+            <tr class="table-primary text-uppercase text-center">
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jenis Kelamin</th>
+                <th>Domisili</th>
+                <th>Program Studi</th>
+                <th>Skill Web & Programming</th>
+            </tr>
+            <?php 
+                if(isset($_POST['submit'])){
+                    $nim = $_POST['nim'];
+                    $nama = $_POST['nama'];
+                    $email = $_POST['email'];
+                    $gender = $_POST['gender'];
+                    $prodi = $_POST['prodi'];
+                    $domisili = $_POST['domisili'];
+                    $skill = $_POST['skill'];
+            ?>
+            <tr class="text-center">
+                <td><?= $nim;?></td>
+                <td><?= $nama;?></td>
+                <td><?= $email;?></td>
+                <td><?= $gender;?></td>
+                <td><?= $domisili;?></td>
+                <td><?= $prodi;?></td>
+                <td><?php foreach($skill as $skill){echo $skill . '<br>';};?></td>
+            </tr>
+            <?php }?>
+        </table>
+    </div>
+</body>
+</html>
---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php 
+$prodi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital"
+];
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$skill = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "JavaScript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50
+];
+
+?>
